3b19c5fe4892accdb03358e64f6c33a097a12e1a
kayehol <kaye.holanda@gmail.com>
2020-05-21 23:00:16 -0300

    fonts

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,9 @@ import Layout from "../components/layout"
 import { graphql } from "gatsby"
 import styled from 'styled-components'
 import Img from "gatsby-image"
+import "typeface-ubuntu"
+import "typeface-vt323"
+import "../styles/global.css"
 
 const Container = styled.div`
     display: flex;
